5015a039ca2180e4af0723fb69a7e467aa8f6ba3
unknown <easytzb@gmail.com>
2014-08-29 17:05:46 +0800

    update from extensin store

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,7 +8,7 @@ var _markJun_ = {
         'vancl': '凡客',
 		'vt': '凡客'
     },
-    backend: 'http://127.0.0.1:89/',
+    backend: 'http://markjun.duapp.com/',
     numOfNotify: 0,
     getKey: function(url) {
         return "data_" + Crypto.MD5(url)
